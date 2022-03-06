46d3a50fd550ca3ef47dda349c96dacbabb759e7
Rayzxxx <ridhosuhendar1@gmail.com>
2022-03-06 12:00:35 +0700

    add new api

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ const PORT = process.env.PORT || 1234
 io.on('connection', (socket) => {
   socket.on('User ID', (id)=>{
     console.log(`user ${id} online`)
-    // socket.join('test')
+    socket.join(id)
   })
   
   socket.on('message', (data) =>{
     socket.emit('message', (data))
+    socket.to(data.sender).emit('message', (data))
     // socket.join('test')
     // socket.to('test').emit('message', (data))
-    // socket.to(data.sender).emit('message', (data))
 
   })
 
