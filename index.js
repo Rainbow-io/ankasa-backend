9dae3aaeba46c6857a722df09de0f98d21e2bae5
Rayzxxx <ridhosuhendar1@gmail.com>
2022-03-06 12:58:22 +0700

    add new api

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ io.on('connection', (socket) => {
   
   socket.on('message', (data) =>{
     socket.emit('message', (data))
+    console.log(data)
     socket.to(data.sender).emit('message', (data))
     // socket.join('test')
     // socket.to('test').emit('message', (data))
