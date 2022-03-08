fdb4c4a035cc8839fca0cb696386b942493cb0c4
Rayzxxx <ridhosuhendar1@gmail.com>
2022-03-08 16:17:44 +0700

    add new api

--- src/controller/index.js
+++ src/controller/index.js
@@ -268,7 +268,7 @@ const bookingTest = async (req, res, next) => {
         const { id, departure, arrival, airline, logo, duration, price, facilies, departure_type, arrival_type, departure_time, arrival_time, transit, classname, date, qty } = req.body
         const idUser = req.params.id
         const data = {
-            id : idUser, departure, arrival, airline, logo, duration, price, facilies, departure_type, arrival_type, departure_time, arrival_time, transit, classname, date, qty
+            id : idUser, departure, arrival, airline, logo, duration, price, facilties, departure_type, arrival_type, departure_time, arrival_time, transit, classname, date, qty
         }
         const process = await model.bookingPost(data)
         standartRespons.respons(res, data, 200, 'api success')
